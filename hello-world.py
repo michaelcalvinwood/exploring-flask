e28d1ef1da55ad589ef2e6051845e207c0267aa2
Michael Wood <michaelwood33311@icloud.com>
2023-10-06 09:14:33 -0400

    added post code snippet and specifying listening address and port

--- hello-world.py
+++ hello-world.py
@@ -1,9 +1,16 @@
 # flask --app hello run
 
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+@app.route('/post', methods=['POST'])
+def post():
+    post_body = request.get_json()
+    print(post_body)
+    return jsonify({"status": "success", "message": 3}), 201
+
---
+++ remote-run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+flask --app $1 run -h 0.0.0.0 -p 3106
